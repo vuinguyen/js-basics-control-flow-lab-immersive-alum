2f5d0ac61fd3821ab9176e99f6285251a12e2980
Vui Nguyen <vui.nguyen@flatironschool.com>
2019-04-25 19:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 199) {
+    return "This one is on me!";
+  } else if (distance >199 && distance < 2501) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do."
+  }
+}
