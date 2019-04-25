2f72e59b6fa8b47dd5bc6cca7b9eae7fcd4c6876
Vui Nguyen <vui.nguyen@flatironschool.com>
2019-04-25 20:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch(tip) {
   case "generous":
-    'Thank you so much.';
+    return 'Thank you so much.';
     break;
   case "not as generous":
-    'Thank you.';
+    return 'Thank you.';
     break;
   default:
     return 'Bye.';
