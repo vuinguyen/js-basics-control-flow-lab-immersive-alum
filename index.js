34710ff49dd4cb512ca9b84a515c2ef8c44a7acc
Vui Nguyen <vui.nguyen@flatironschool.com>
2019-04-25 20:32:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 199) {
+    return "This one is on me!";
+  } else if (distance >199 && distance < 2501) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  return greeting = (city == 'NYC') ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+  case "generous":
+    return 'Thank you so much.';
+    break;
+  case "not as generous":
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+  }
+}
